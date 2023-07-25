7554056c9d7f269898bd3368e1e9582baa67373f
Mani <mani.ebra@gmail.com>
2023-07-25 10:59:35 +0330

    designed app ui

---
+++ core/view.js
@@ -0,0 +1,6 @@
+let addTestcaseButton = document.getElementById("addTestcaseButton");
+let testcases = document.getElementById("testcases");
+
+addTestcaseButton.addEventListener("click", () => {
+  testcases.innerHTML += "123";
+});
